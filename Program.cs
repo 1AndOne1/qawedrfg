01ebf590b9d7502e833b881e91cfb9c2da9364f9
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2023-09-25 07:17:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,79 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-
-// Класс Videocard
-public class Videocard
+﻿public partial class Program
 {
-    public string Model { get; set; }
-    public int Type { get; set; }
-    public string Maker { get; set; }
-
-    // Конструктор класса Videocard
-    public Videocard(string model, int type, string maker)
+    static void Main(int b = 10, int a = 1, int x = 1)
     {
-        Model = Model;
-        Type = type;
-        Maker = maker;
-    }
-}
+        int[][] mass = new int[b - a][];
 
-// Класс для списка видеокарт
-public class Videocardlist
-{
-    private List <Videocard> videocards;
-
-    // Конструктор класса Videocardlist
-    public Videocardlist()
-    {
-        videocards = new List<Videocard>();
-    }
-
-    //Добавление новой видеокарты в список
-    public void AddVideocard(Videocard videocard)
-    {
-        videocards.Add(videocard);
-    }
-
-    // Метод для вывода списка всех видеокарт
-    public void ShowVideoCards()
-    {
-        if (videocards.Count == 0)
+        for (int k = 0; a < b; k++)
         {
-            Console.WriteLine("Видеокарт нет в наличии");
-        }
-        else
-        {
-            Console.WriteLine("Информация о видеокарте: ");
-
-            foreach (Videocard videocard in videocards)
+            int[] arr = new int[9];
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("Название видеокарты: " + videocard.Model);
-                Console.WriteLine("Тип видеокарты: " + videocard.Type);
-                Console.WriteLine("Производитель: " + videocard.Maker);
-                Console.WriteLine();
+                arr[i] = a * x;
+                mass[i] = arr;
+                Console.Write(arr[i] + " ");
+                x++;
             }
+            Console.WriteLine();
+            a++;
+            x = 1;
         }
+        Console.Write(mass[3][4]);
     }
-}
 
-// Главный класс программы
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // Создание экземпляра класса Videocardlist
-        Videocardlist videocardlist = new Videocardlist();
-
-        // Добавление видеокарт в список
-        Videocard videocard1 = new Videocard("GTX", 1070, "MSI");
-        Videocard videocard2 = new Videocard("RTX", 3070, "Gigabyte");
-        Videocard videocard3 = new Videocard("Radeon RX", 6600, "AMD");
-
-        videocardlist.AddVideocard(videocard1);
-        videocardlist.AddVideocard(videocard2);
-        videocardlist.AddVideocard(videocard3);
-
-        // Вывод списка видеокарт
-        videocardlist.ShowVideoCards();
-    }
 }
