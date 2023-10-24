70c9125493d975b9f0faa0bdd0d8bf752e0475ed
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2023-10-24 10:15:25 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,3 +22,23 @@
     }
 
 }
+
+int[][] field = new int [10][];
+for (int x = 0; x < 10; x++) {
+    int[] crossField = new int[10];
+    for (int i = 0; i < crossField.Length; i++) {
+        crossField[i] = 0;
+        Console.WriteLine(crossField[i]);
+        x++;
+    }
+    field[x] = crossField;
+    Console.WriteLine();
+}
+void garden1() {
+    int gar = 1;
+    for (int i = 0; i < 10; i++) {
+        gar = gar + i;
+        field[gar][] = field[][];
+    }
+}
+
